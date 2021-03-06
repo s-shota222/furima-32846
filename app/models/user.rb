bbe92e5189a446fb48f737e953f35ae8ad596257
s-shota222 <yowamuahi222@yahoo.co.jp>
2021-03-06 12:16:27 +0900

    ログイン時とログアウト時のviewの表示設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+=begin
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end 
+=end
 
 end
