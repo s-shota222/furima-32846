876861e6408e576872a1e1a1f9a1797819977be5
s-shota222 <yowamuahi222@yahoo.co.jp>
2021-03-06 15:38:19 +0900

    バリデーション の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-=begin
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  PASSWORD_REGEX_name = /\A[ぁ-んァ-ン一-龥]/.freeze
+  validates_format_of :first_name,:last_name,  with: PASSWORD_REGEX_name, message: 'は全角（漢字・ひらがな・カタカナ）で設定してください'
+  PASSWORD_REGEX_ruby = /\A[ァ-ヶー－]+\z/.freeze
+  validates_format_of :first_name_ruby,:last_name_ruby,  with: PASSWORD_REGEX_ruby, message: 'は全角（カタカナ）で設定してください'
+
   with_options presence: true do
     validates :nickname
     validates :email, uniqueness: true
-    validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :encrypted_password 
+    validates :first_name 
+    validates :last_name
+    validates :first_name_ruby
+    validates :last_name_ruby
     validates :birthday
   end 
-=end
 
 end
