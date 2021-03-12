009ee8ef49afab7f076e0acfd385fa482f2ee8a6
s-shota222 <yowamuahi222@yahoo.co.jp>
2021-03-12 21:31:33 +0900

    バリデーションの記述を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください'
-  PASSWORD_REGEX_name = /\A[ぁ-んァ-ン一-龥]/.freeze
+  PASSWORD_REGEX_name = /\A[ぁ-んァ-ン一-龥々]/.freeze
   validates_format_of :first_name, :last_name, with: PASSWORD_REGEX_name, message: 'は全角（漢字・ひらがな・カタカナ）で設定してください'
   PASSWORD_REGEX_ruby = /\A[ァ-ヶー－]+\z/.freeze
   validates_format_of :first_name_ruby, :last_name_ruby, with: PASSWORD_REGEX_ruby, message: 'は全角（カタカナ）で設定してください'
