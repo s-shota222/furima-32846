2bfe1abf811cb7ec537f9fad969fa544f6d00ed5
s-shota222 <yowamuahi222@yahoo.co.jp>
2021-03-13 17:17:24 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :information).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :prefecture, :delivery_fee, :days_to_ship
+
+  validates :category_id, :condition_id, :prefecture_id, :delivery_fee_id, :days_to_ship_id, numericality: { other_than: 1 }
 end
