bae10b351a7c72566761b034b53847a6e60cf6dc
s-shota222 <yowamuahi222@yahoo.co.jp>
2021-03-21 11:14:38 +0900

    レビュー修正(2)

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,6 @@ FactoryBot.define do
     prefecture_id    { 2 }
     days_to_ship_id  { 2 }
     price            { 8000 }
-    user_id          { 4 }
     association :user
 
     after(:build) do |item|
