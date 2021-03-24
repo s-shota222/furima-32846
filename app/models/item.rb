1aa5dcd5caa923550ca38a1a64ccbbc68fbb16ce
s-shota222 <yowamuahi222@yahoo.co.jp>
2021-03-24 21:41:22 +0900

    商品一覧表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :prefecture, :delivery_fee, :days_to_ship
-  
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :delivery_fee
+  belongs_to :days_to_ship
+
   validates :category_id, :condition_id, :prefecture_id, :delivery_fee_id, :days_to_ship_id, numericality: { other_than: 1 }
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
   with_options presence: true do
     validates :image
     validates :name
