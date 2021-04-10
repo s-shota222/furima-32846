191bd56d11d8b24b8b4bd7336a55705d773b51b7
s-shota222 <yowamuahi222@yahoo.co.jp>
2021-04-10 14:59:19 +0900

    paramsで情報を取得できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,16 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    Order.create(order_params)
+    ShippingAddress.create(shipping_address_params)
+  end
+
+  private
+  def order_params
+    params.merge(item_id: item.id, user_id: current_user.id)
+  end
+
+  def shipping_address_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(order_id: order.id)
   end
 end
